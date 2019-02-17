5cb8d4915c145d0f84fe2161a0633b3aa8493b31
bigfood <satcom1600@hanmail.net>
2019-02-18 01:41:35 +0900

    Write hello world

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,23 @@
-console.log('hello world');
+import Koa from 'koa';
+import Router from 'koa-router';
+
+const serverPort = 3000;
+
+const app = new Koa();
+const router = new Router();
+
+app.use(async (ctx, next) => {
+    // Log the request to the console
+    console.log('Url:', ctx.url);
+    // Pass the request to the next middleware function
+    await next();
+});
+
+router.get('/*', async (ctx) => {
+    ctx.body = 'Hello World!';
+});
+app.use(router.routes());
+
+app.listen(serverPort);
+
+console.log(`Server running on port ${serverPort}`);
