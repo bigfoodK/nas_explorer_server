1f2c13ce2656ed2e89df655afc02ebbdaa452d2a
bigfood <satcom1600@hanmail.net>
2019-02-18 02:15:32 +0900

    Divide router from server.ts to router.ts

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,14 @@
 import Koa from 'koa';
-import Router from 'koa-router';
 import { config } from './config';
 import logger from './logger';
+import { router } from './router';
 
 const serverPort = config.port;
 
 const app = new Koa();
-const router = new Router();
 
 app.use(logger);
 
-router.get('/*', async (ctx) => {
-    ctx.body = 'Hello World!';
-});
 app.use(router.routes());
 
 app.listen(serverPort);
