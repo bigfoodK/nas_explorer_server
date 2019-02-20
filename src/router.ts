39d43edec40650d6bbb3a1e958840c9cc9501beb
bigfood <satcom1600@hanmail.net>
2019-02-21 01:23:45 +0900

    Add serveIndex.ts to browse server

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,15 @@
 import Router from 'koa-router';
 import serveData from './serveData';
+import serveIndexes from './serveIndex';
 
 const router = new Router();
 
 router.get('/', async (ctx, next) => {
-  ctx.params.path = 'hello.html';
-  await serveData(ctx, next);
+  ctx.redirect('/explore');
 });
 
 router.get('/data/:path*', serveData);
+
+router.get('/index/:path*', serveIndexes);
 
 export { router };
---
+++ src/serveIndex.ts
@@ -0,0 +1,109 @@
+import fs from 'fs';
+import Koa from 'koa';
+import path, { extname } from 'path';
+import { config } from './config';
+
+type FileIndex = {
+  type: ('directory' | 'text' | 'image' | 'audio' | 'video' | 'binary');
+  name: string;
+  path: string;
+  size: number;
+  createdAtMs: Date;
+  modifiedAtMs: Date;
+}
+
+const rootDir = config.dataRoot;
+
+export default async function serveData(ctx: Koa.Context, next: () => Promise<any>) {
+  const normalizedPath = path.normalize(ctx.params.path || '/');
+  const isUpperPath = normalizedPath.startsWith('../') || normalizedPath.startsWith('..\\') || normalizedPath === '..';
+
+  if(isUpperPath) {
+    ctx.status = 403;
+    return;
+  }
+
+  const directoryPath = path.join(rootDir, normalizedPath);
+  const stat = await getDirectoryStat(directoryPath);
+
+  if(!stat) {
+    ctx.status = 404;
+    return;
+  }
+
+  const directoryEntries = await fs.promises.readdir(directoryPath);
+  const promisesMakingFileIndex: Promise<void>[] = [];
+  const fileIndexes: FileIndex[] = [];
+
+  directoryEntries.forEach(fileName => {
+    const filePath = path.join(directoryPath, fileName);
+    
+    promisesMakingFileIndex.push(
+      fs.promises.stat(filePath)
+      .then(fileStats => {
+        const type = fileStats.isDirectory()
+          ? 'directory' 
+          : identifyFileType(fileName);
+        
+        fileIndexes.push({
+          type: type,
+          name: fileName,
+          path: filePath,
+          size: fileStats.size,
+          createdAtMs: fileStats.birthtime,
+          modifiedAtMs: fileStats.mtime,
+        });
+      })
+    );
+  });
+
+  await Promise.all(promisesMakingFileIndex);
+
+  sendIndex(ctx, fileIndexes);
+}
+
+async function getDirectoryStat(path: string) {
+  try {
+    const stats = await fs.promises.stat(path);
+
+    if (!stats.isDirectory()) return false;
+    return stats;
+
+  } catch (e) {
+    switch (e.code) {
+      case 'ENOENT':
+        return false;
+    
+      default:
+        throw e;
+    }
+  }
+}
+
+function sendIndex(ctx: Koa.Context, fileIndexes: FileIndex[]) {
+  ctx.set('Content-Type', 'application/json');
+  ctx.body = fileIndexes;
+}
+
+function identifyFileType(name: string) {
+  const extensionName = path.extname(name);
+
+  switch (extensionName) {
+    case '.txt':
+    case '.html':
+    return 'text';
+
+    case '.jpg':
+    case '.png':
+    return 'image';
+
+    case '.mp3':
+    return 'audio';
+
+    case '.mp4':
+    return 'video';
+
+    default:
+    return 'binary';
+  }
+}
